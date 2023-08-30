9123baeb5b6d7426b816a9c0c0a5e61ad18189cb
Paultje52 <Paultje52@gmail.com>
2023-08-30 23:10:32 +0000

    Added viewport fix cover

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,10 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Scoreboard</title>
         <meta name="description" content="A scoreboard app without ads" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 viewport-fit=cover"
+        />
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="manifest" href="/manifest.json" />
