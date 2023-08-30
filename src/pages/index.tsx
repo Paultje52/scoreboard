ee790f7525495494d420366c565ccccd1aa843ed
Paultje52 <Paultje52@gmail.com>
2023-08-30 22:07:23 +0000

    Added edit mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,59 @@
 import useLocalStorageState from "@/components/useLocalStorageState";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
 export default function Home() {
   const [blueScore, setBlueScore] = useLocalStorageState(0, "blueScore");
   const [redScore, setRedScore] = useLocalStorageState(0, "redScore");
 
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const addBlueScore = () => {
+    if (isEditing || blueScore >= 99) return;
+    setBlueScore(blueScore + 1);
+  };
+  const overrideBlueScore = (score: number) => {
+    if (score < 0 || score >= 99) return;
+    setBlueScore(score);
+  };
+
+  const addRedScore = () => {
+    if (isEditing || redScore >= 99) return;
+    setRedScore(redScore + 1);
+  };
+  const overrideRedScore = (score: number) => {
+    if (score < 0 || score >= 99) return;
+    setRedScore(score);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.scores}>
-        <div
-          className={styles.score}
-          onClick={() => {
-            setBlueScore(blueScore + 1);
-          }}
-        >
-          {blueScore}
+        <div className={styles.score} onClick={addBlueScore}>
+          {isEditing && (
+            <span onClick={() => overrideBlueScore(blueScore + 1)}>+</span>
+          )}
+          <p>{blueScore}</p>
+          {isEditing && (
+            <span onClick={() => overrideBlueScore(blueScore - 1)}>-</span>
+          )}
         </div>
-        <div
-          className={styles.score}
-          onClick={() => {
-            setRedScore(redScore + 1);
-          }}
-        >
-          {redScore}
+        <div className={styles.score} onClick={addRedScore}>
+          {isEditing && (
+            <span onClick={() => overrideRedScore(redScore + 1)}>+</span>
+          )}
+          <p>{redScore}</p>
+          {isEditing && (
+            <span onClick={() => overrideRedScore(redScore - 1)}>-</span>
+          )}
         </div>
       </div>
       <div className={styles.text}>
         <div>
-          <a>Edit</a>
+          <a onClick={toggleEdit}>{isEditing ? "Done" : "Edit"}</a>
           <a>Reset</a>
         </div>
         <div>
