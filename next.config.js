51f85c9cb6f5891cb47ccc4ebfa65acf3b6deb90
Paultje52 <Paultje52@gmail.com>
2023-08-30 22:41:47 +0000

    Added next-pwa & webmanifest Scoreboard is now PWA-ready!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withPWA = require("next-pwa")({
+  dest: "public"
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   output: "export"
-};
+});
 
 module.exports = nextConfig;
