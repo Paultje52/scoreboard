9231fce64bc966f5ca9bcf23681e4b8748c33eb5
Paultje52 <Paultje52@gmail.com>
2023-08-30 21:28:57 +0000

    Added base HTML & position styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,22 @@ import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
-      <p>Hello, world!</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.scores}>
+        <div className={styles.score}>0</div>
+        <div className={styles.score}>0</div>
+      </div>
+      <div className={styles.text}>
+        <div>
+          <a>Edit</a>
+          <a>Reset</a>
+        </div>
+        <div>
+          <a href="https://github.com/Paultje52/scoreboard" target="_blank">
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
